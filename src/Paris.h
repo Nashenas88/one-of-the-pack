716c202a3cccb413208d52800b2bedcaff42eb30
Nashenas88 <Nashenas88@588f7fc3-8d45-8d75-f275-5bb5471f4c86>
2010-04-27 21:26:37 +0000

    Added Paris class (the girl blob) to the code.

---
+++ src/Paris.h
@@ -0,0 +1,19 @@
+#ifndef FLOCK__PARIS__H
+#define FLOCK__PARIS__H
+
+#include "special.h"
+#include "defines.h"
+
+class Ahnold : public Special
+{  
+public:
+  Paris(void);
+  Paris(float x, float y, int map_x, int map_y, int num, int frames,
+         int abil_frames, Texture *tex, direc dir, FMOD_SYSTEM *sys,
+         FMOD_SOUND *music, FMOD_CHANNEL *ch, FMOD_SOUND *as, FMOD_CHANNEL *ac);
+  
+  void use_ability(Map *m);
+  void enable_ability(Map *m);
+};
+
+#endif // FLOCK__PARIS__H
