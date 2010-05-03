1b2f2124ecc81d658dac89a6e04c3f59939c7c1d
Nashenas88 <Nashenas88@588f7fc3-8d45-8d75-f275-5bb5471f4c86>
2010-05-03 00:23:45 +0000

    Removed unnecesary level image in the main resources folder. Also cleaned up some of the main menu state code so it only includes the necessary information.

--- trunk/src/main_menu_state.cpp
+++ trunk/src/main_menu_state.cpp
@@ -1,42 +1,20 @@
 #include "main_menu_state.h"
 
 Main_Menu_State::Main_Menu_State(void)
-: State(), dspparameq(0) {}
+: State(), {}
 
-Main_Menu_State::Main_Menu_State(FMOD_SYSTEM *system, FMOD_DSP *pe, Game_State *gs,
-              Drawable *b, Drawable *m, Drawable *p, Texture *icons)
-:State(system), dspparameq(pe), game_state(gs), background(b),
-map(m), pointer(p), selected(0), anim_timer(0)
-{
-  movers = new Drawable (0.0f, 0.0f, 1, 1, MAP_ICON, icons);
-  statics = new Drawable (0.0f, 0.0f, 1, 1, MAP_ICON, icons);
-}
+Main_Menu_State::Main_Menu_State(FMOD_SYSTEM *system, Drawable *b, Drawable *p)
+:State(system), background(b), pointer(p), selected(0)
+{}
 
 void Main_Menu_State::draw(void)
 {
-  
-  
+  background->draw();
   pointer->draw();
 }
 
 void Main_Menu_State::update(int &delta)
 {
-  if (anim_timer == PAUSE_ANIM_DELAY)
-  {
-    anim_timer = 0;
-    int current;
-    current = movers->get_cur_frame();
-    ++current;
-    if (current > PAUSE_ICON_FRAMES)
-    {
-      current = 1;
-    }
-    movers->set_cur_frame(current);
-  }
-  else
-  {
-    ++anim_timer;
-  }
   state_update();
 }
 
@@ -104,11 +82,7 @@ void Main_Menu_State::reset_selected(void)
 void Main_Menu_State::clean(void)
 {
   background->clean();
-  map->clean();
   pointer->clean();
   delete background;
-  delete map;
   delete pointer;
-  game_state->clean();
-  delete game_state;
 }
